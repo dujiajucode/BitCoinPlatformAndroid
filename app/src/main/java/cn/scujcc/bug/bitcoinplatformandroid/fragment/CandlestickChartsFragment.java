9cf645b79f04b693de7c46ca42033a5fe4b78e74
lilujia <dujiajucode@163.com>
2016-03-28 19:37:41 +0800

    添加K线图

--- app/src/main/java/cn/scujcc/bug/bitcoinplatformandroid/fragment/CandlestickChartsFragment.java
+++ app/src/main/java/cn/scujcc/bug/bitcoinplatformandroid/fragment/CandlestickChartsFragment.java
@@ -203,41 +203,11 @@
  *    limitations under the License.
  *
  */
+
 package cn.scujcc.bug.bitcoinplatformandroid.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import cn.scujcc.bug.bitcoinplatformandroid.R;
-
 /**
- * Created by lilujia on 16/3/27.
- * <p/>
- * 行情资讯
+ * Created by lilujia on 16/3/28.
  */
-public class QuotationInformationFragment extends BaseFragment {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_hello, container, false);
-
-        setHasOptionsMenu(true);
-
-        getActivity().setTitle("行情资讯");
-
-        TextView tv = (TextView) view.findViewById(R.id.fragment_hello_textview);
-        tv.setText("行情资讯");
-
-
-        return view;
-    }
+public class CandlestickChartsFragment extends BaseFragment {
 }

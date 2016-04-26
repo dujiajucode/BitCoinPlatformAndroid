e0b8694d4c5c6e865b6c064b5e4ef58793ed581f
lilujia <dujiajucode@163.com>
2016-04-26 17:49:59 +0800

    更换Logo

--- app/src/main/java/cn/scujcc/bug/bitcoinplatformandroid/fragment/ActualTransactionCandlestickChartsFragment.java
+++ app/src/main/java/cn/scujcc/bug/bitcoinplatformandroid/fragment/ActualTransactionCandlestickChartsFragment.java
@@ -183,21 +183,37 @@
  */
 package cn.scujcc.bug.bitcoinplatformandroid.fragment;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.CandleStickChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.CandleData;
+import com.github.mikephil.charting.data.CandleDataSet;
+import com.github.mikephil.charting.data.CandleEntry;
+
+import java.util.ArrayList;
 
 import cn.scujcc.bug.bitcoinplatformandroid.R;
 
 /**
  * Created by lilujia on 16/3/27.
- * <p>
+ * <p/>
  * 个人中心
  */
-public class ActualTransactionCandlestickChartsFragment extends BaseFragment {
+public class ActualTransactionCandlestickChartsFragment extends BaseFragment implements SeekBar.OnSeekBarChangeListener {
+
+    private CandleStickChart mChart;
+    private SeekBar mSeekBarX, mSeekBarY;
+    private TextView tvX, tvY;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -209,13 +225,113 @@ public class ActualTransactionCandlestickChartsFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_actualtransactioncandlestickcharts, container, false);
 
+        tvX = (TextView) view.findViewById(R.id.tvXMax);
+        tvY = (TextView) view.findViewById(R.id.tvYMax);
 
-        TextView tv = (TextView) view.findViewById(R.id.fragment_hello_textview);
-        tv.setText("综合");
+        mSeekBarX = (SeekBar) view.findViewById(R.id.seekBar1);
+        mSeekBarX.setOnSeekBarChangeListener(this);
 
+        mSeekBarY = (SeekBar) view.findViewById(R.id.seekBar2);
+        mSeekBarY.setOnSeekBarChangeListener(this);
+
+        mChart = (CandleStickChart) view.findViewById(R.id.chart1);
+        mChart.setBackgroundColor(Color.WHITE);
+
+        mChart.setDescription("");
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        mChart.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        mChart.setPinchZoom(false);
+
+        mChart.setDrawGridBackground(false);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setSpaceBetweenLabels(2);
+        xAxis.setDrawGridLines(false);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+//        leftAxis.setEnabled(false);
+        leftAxis.setLabelCount(7, false);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setDrawAxisLine(false);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+//        rightAxis.setStartAtZero(false);
+
+        // setting data
+        mSeekBarX.setProgress(40);
+        mSeekBarY.setProgress(100);
+
+        mChart.getLegend().setEnabled(false);
+
+        mChart.animateX(3000);
 
         return view;
     }
 
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int prog = (mSeekBarX.getProgress() + 1);
+
+        tvX.setText("" + prog);
+        tvY.setText("" + (mSeekBarY.getProgress()));
+
+        mChart.resetTracking();
+
+        ArrayList<CandleEntry> yVals1 = new ArrayList<CandleEntry>();
+
+        for (int i = 0; i < prog; i++) {
+            float mult = (mSeekBarY.getProgress() + 1);
+            float val = (float) (Math.random() * 40) + mult;
+
+            float high = (float) (Math.random() * 9) + 8f;
+            float low = (float) (Math.random() * 9) + 8f;
+
+            float open = (float) (Math.random() * 6) + 1f;
+            float close = (float) (Math.random() * 6) + 1f;
+
+            boolean even = i % 2 == 0;
+
+            yVals1.add(new CandleEntry(i, val + high, val - low, even ? val + open : val - open,
+                    even ? val - close : val + close));
+        }
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < prog; i++) {
+            xVals.add("" + (1990 + i));
+        }
+
+        CandleDataSet set1 = new CandleDataSet(yVals1, "Data Set");
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+//        set1.setColor(Color.rgb(80, 80, 80));
+        set1.setShadowColor(Color.DKGRAY);
+        set1.setShadowWidth(0.7f);
+        set1.setDecreasingColor(Color.RED);
+        set1.setDecreasingPaintStyle(Paint.Style.FILL);
+        set1.setIncreasingColor(Color.rgb(122, 242, 84));
+        set1.setIncreasingPaintStyle(Paint.Style.STROKE);
+        set1.setNeutralColor(Color.BLUE);
+        //set1.setHighlightLineWidth(1f);
+
+        CandleData data = new CandleData(xVals, set1);
+
+        mChart.setData(data);
+        mChart.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
 }
